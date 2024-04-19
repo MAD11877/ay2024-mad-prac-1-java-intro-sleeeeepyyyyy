e05a9156fd1f7ff860223ab2087f4bcfbf6552b9
sleeeeepyyyyy <167509489+sleeeeepyyyyy@users.noreply.github.com>
2024-04-19 23:10:41 +0800

    Question2.java

--- Question2.java
+++ Question2.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 public class Question2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double[] inputs = getUserInputs(in);
-        double bmi = calculateBMI(inputs[0], inputs[1]);
+        
+        double height = getUserInput(in, "Enter your height in meters (m): ");
+        double weight = getUserInput(in, "Enter your weight in kilograms (kg): ");
+        
+        double bmi = calculateBMI(height, weight);
+        
         System.out.println("Your BMI is: " + bmi);
         in.close();
     }
-
-    static double[] getUserInputs(Scanner scanner) {
-        System.out.print("Enter your height in meters (m): ");
-        double height = scanner.nextDouble();
-        
-        System.out.print("Enter your weight in kilograms (kg): ");
-        double weight = scanner.nextDouble();
-        
-        return new double[]{height, weight};
+    
+    static double getUserInput(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        return scanner.nextDouble();
     }
-
+    
     static double calculateBMI(double height, double weight) {
         return weight / (height * height);
     }
 }
+
