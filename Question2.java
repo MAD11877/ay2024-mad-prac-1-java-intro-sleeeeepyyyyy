12d658b96d25575b1e800769d0c907f16aa570c2
sleeeeepyyyyy <167509489+sleeeeepyyyyy@users.noreply.github.com>
2024-04-21 23:18:35 +0800

    Question2.java

--- Question2.java
+++ Question2.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Question2 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner in = new Scanner(System.in);
         
         System.out.print("Enter your height in meters (m): ");
