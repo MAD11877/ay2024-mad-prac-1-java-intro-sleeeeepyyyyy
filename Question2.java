34de77821f1dd24431ff7ecbb6497ec631c26cfe
sleeeeepyyyyy <167509489+sleeeeepyyyyy@users.noreply.github.com>
2024-04-22 00:05:14 +0800

    Question2.java

--- Question2.java
+++ Question2.java
@@ -6,15 +6,15 @@ public class Question2
     {
         Scanner in = new Scanner(System.in);
         
-        System.out.println("Enter your height in meters (m): ");
-        double height = in.nextDouble(); 
+        System.out.print("Enter your height in meters: ");
+        double height = in.nextDouble();
         
-        System.out.println("Enter your weight in kilograms (kg): ");
-        double weight = in.nextDouble(); 
+        System.out.print("Enter your weight in kilograms: "); 
+        double weight = in.nextDouble();
         
         double bmi = weight / (height * height); 
         
-        System.out.println(bmi);
+        System.out.println(bmi); 
         
         in.close(); 
     }
